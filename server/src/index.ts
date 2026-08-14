e5321a59532f0dfe5a1a4aca8bbb47defa6daebc
monushah108 <sonuk627422@gmail.com>
2026-08-14 16:08:33 +0530

    wip:server update

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ function init() {
 
   const io = new Server(httpServer, {
     cors: {
-      origin: "http://localhost:3001",
+      origin: process.env.CLIENT_URL,
       credentials: true,
     },
   });
