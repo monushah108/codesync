b7de90b52246db5842192026964c83cabe4edfce
monushah108 <sonuk627422@gmail.com>
2026-09-16 21:58:00 +0530

    bug:error fix

--- lib/webcontainer.ts
+++ lib/webcontainer.ts
@@ -6,62 +6,62 @@ export async function getWebContainer() {
 
 const webcontainer = await getWebContainer();
 
-webcontainer().mount({
-  "index.html": {
-    file: {
-      contents: "html",
-    },
-  },
+// webcontainer().mount({
+//   "index.html": {
+//     file: {
+//       contents: "html",
+//     },
+//   },
 
-  "style.css": {
-    file: {
-      contents: "css",
-    },
-  },
+//   "style.css": {
+//     file: {
+//       contents: "css",
+//     },
+//   },
 
-  "script.js": {
-    file: {
-      contents: "js",
-    },
-  },
+//   "script.js": {
+//     file: {
+//       contents: "js",
+//     },
+//   },
 
-  "server.js": {
-    file: {
-      contents: `
-        const http = require("http");
-        const fs = require("fs");
-        const path = require("path");
+//   "server.js": {
+//     file: {
+//       contents: `
+//         const http = require("http");
+//         const fs = require("fs");
+//         const path = require("path");
 
-        const server = http.createServer((req, res) => {
-          let filePath = req.url === "/"
-            ? "./index.html"
-            : "." + req.url;
+//         const server = http.createServer((req, res) => {
+//           let filePath = req.url === "/"
+//             ? "./index.html"
+//             : "." + req.url;
 
-          if (!fs.existsSync(filePath)) {
-            res.writeHead(404);
-            res.end("Not Found");
-            return;
-          }
+//           if (!fs.existsSync(filePath)) {
+//             res.writeHead(404);
+//             res.end("Not Found");
+//             return;
+//           }
 
-          const ext = path.extname(filePath);
+//           const ext = path.extname(filePath);
 
-          const types = {
-            ".html": "text/html",
-            ".css": "text/css",
-            ".js": "text/javascript",
-          };
+//           const types = {
+//             ".html": "text/html",
+//             ".css": "text/css",
+//             ".js": "text/javascript",
+//           };
 
-          res.writeHead(200, {
-            "Content-Type": types[ext] || "text/plain",
-          });
+//           res.writeHead(200, {
+//             "Content-Type": types[ext] || "text/plain",
+//           });
 
-          fs.createReadStream(filePath).pipe(res);
-        });
+//           fs.createReadStream(filePath).pipe(res);
+//         });
 
-        server.listen(3000, "0.0.0.0");
-      `,
-    },
-  },
-});
+//         server.listen(3000, "0.0.0.0");
+//       `,
+//     },
+//   },
+// });
 
 await webcontainer.spawn("npx", ["serve", ".", "-l", "3000"]);
